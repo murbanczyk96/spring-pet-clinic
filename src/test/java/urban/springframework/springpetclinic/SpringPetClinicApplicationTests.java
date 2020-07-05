07ba121d2c337af5b8ee3fa82030891d1da45373
urbanq <m010996@gmail.com>
2020-07-05 16:08:28 +0200

    Initial Project Add

---
+++ src/test/java/urban/springframework/springpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package urban.springframework.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
