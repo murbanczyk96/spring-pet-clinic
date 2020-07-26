73ee8ff2ce48ba226eb4fc0aba4fcb7c65d0aa27
urbanq <m010996@gmail.com>
2020-07-26 15:55:26 +0200

    Added PetType Map Service Impl. Closes #29

---
+++ pet-clinic-data/src/main/java/urban/springframework/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package urban.springframework.springpetclinic.services;
+
+import urban.springframework.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
