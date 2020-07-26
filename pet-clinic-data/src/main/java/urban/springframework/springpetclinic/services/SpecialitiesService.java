7f9feb9f315d2dea9c13810bbe51c96a76101a9c
urbanq <m010996@gmail.com>
2020-07-26 19:45:18 +0200

    added Specialties Map Implementation . Closes #31

---
+++ pet-clinic-data/src/main/java/urban/springframework/springpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package urban.springframework.springpetclinic.services;
+
+import urban.springframework.springpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/urban/springframework/springpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,36 @@
+package urban.springframework.springpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import urban.springframework.springpetclinic.model.Speciality;
+import urban.springframework.springpetclinic.services.SpecialitiesService;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
