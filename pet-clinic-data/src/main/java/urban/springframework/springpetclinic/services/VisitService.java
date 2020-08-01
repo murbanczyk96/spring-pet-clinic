168e284951cf702d20fd2ef85087e64ce0c57244
urbanq <m010996@gmail.com>
2020-08-02 01:08:49 +0200

    Added Map implementation for Visit Service. Closes #46

---
+++ pet-clinic-data/src/main/java/urban/springframework/springpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package urban.springframework.springpetclinic.services;
+
+import urban.springframework.springpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
