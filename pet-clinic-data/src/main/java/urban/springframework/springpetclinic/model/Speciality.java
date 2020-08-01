b2426c47a74680ea049fa10e31a12d5f1827dc4d
urbanq <m010996@gmail.com>
2020-08-01 22:52:20 +0200

    Updated vet with JPA mappings . Closes #37

--- pet-clinic-data/src/main/java/urban/springframework/springpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/urban/springframework/springpetclinic/model/Speciality.java
@@ -12,6 +12,7 @@ public class Speciality extends BaseEntity {
     @Column(name = "description")
     private String description;
 
+
     public String getDescription() {
         return description;
     }
--- pet-clinic-data/src/main/java/urban/springframework/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/urban/springframework/springpetclinic/model/Vet.java
@@ -13,6 +13,7 @@ public class Vet extends Person{
             inverseJoinColumns = @JoinColumn("speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
+
     public Set<Speciality> getSpecialities() {
         return specialities;
     }
