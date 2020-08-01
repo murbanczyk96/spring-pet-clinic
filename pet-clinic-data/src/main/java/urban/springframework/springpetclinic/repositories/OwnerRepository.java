304a3218f6c2f25700ea844e7316b8fc80c78e72
urbanq <m010996@gmail.com>
2020-08-02 00:15:27 +0200

    Added Spring Data JPA Service for Owner . Closes #40

--- pet-clinic-data/src/main/java/urban/springframework/springpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/urban/springframework/springpetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import urban.springframework.springpetclinic.model.Owner;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
