6aaba612f8dd89f0bc62f781f335d3db96ddf3b8
urbanq <m010996@gmail.com>
2020-08-07 19:49:53 +0200

    Added MockMvc test for OwnerController. Closes #52

--- pet-clinic-web/src/main/java/urban/springframework/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/urban/springframework/springpetclinic/controllers/OwnerController.java
@@ -16,7 +16,7 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"", "/", "/owners", "/index", "/index.html"})
+    @RequestMapping({"", "/", "/index", "/index.html"})
     public String listOwners(Model model){
 
         model.addAttribute("owners", ownerService.findAll());
