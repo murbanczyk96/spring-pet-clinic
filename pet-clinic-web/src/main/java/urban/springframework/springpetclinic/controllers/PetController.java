d24eccd8718934446007dbafab7c974a869a74b8
urbanq <m010996@gmail.com>
2020-08-15 20:01:36 +0200

    Fixed parse issue on PetType. Closes #65

--- pet-clinic-web/src/main/java/urban/springframework/springpetclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/urban/springframework/springpetclinic/controllers/PetController.java
@@ -51,6 +51,7 @@ public class PetController {
     @GetMapping("/pets/new")
     public String initCreationForm(Owner owner, Model model){
         Pet pet = Pet.builder().build();
+        pet.setOwner(owner);
         owner.getPets().add(pet);
         model.addAttribute("pet", pet);
         return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
